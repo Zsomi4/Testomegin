f2599c9b3f23dd31744149cebe71dbd94fb77776
Zsomi4 <k72302872172@ktch.hu>
2022-05-26 09:13:49 +0200

    űrlap javítása és a calc.php megírása

--- calc.php
+++ calc.php
@@ -9,3 +9,35 @@ Date: 2022-05-26
 Github: https://github.com/Zsomi4/
 Licenc: GNU GPL
  */
+
+echo file_get_contents('templates/head.html');
+
+
+$page = file_get_contents('templates/calc.html');
+
+function calcWeight($weight, $height) {    
+    $final = $weight/pow($height, 2);
+    return $final;
+}
+
+
+if(
+    !empty($_GET['weight']) and 
+    !empty($_GET['height']) 
+) {
+    $weight = $_GET['weight'];
+    $height = $_GET['height'];
+    $final = calcWeight($weight, $height);    
+}else {
+    $final = "Nem hagyhatod üresen a két mezőt!";
+}
+
+
+
+
+
+$page = str_replace('{{ result }}', $area, $page);
+
+echo $page;
+
+echo file_get_contents('templates/foot.html');
