3856f3462e354d032f601ade47289b723c002730
Zsomi4 <k72302872172@ktch.hu>
2022-05-26 09:05:43 +0200

    Névjegy weboldal elkészítése

--- about.php
+++ about.php
@@ -8,3 +8,7 @@ Date: 2022-05-26
 Github: https://github.com/Zsomi4/
 Licenc: GNU GPL
  */
+
+echo file_get_contents('templates/head.html');
+echo file_get_contents('templates/about.html');
+echo file_get_contents('templates/foot.html');
